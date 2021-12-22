dcd0351c36091c008435a93d9232bb7d432eb604
pankaj <pankaj.sikar@gmail.com>
2021-12-22 09:28:59 +0530

    "Add docker-compose file for locust"

--- src/locust/locustfile.py
+++ src/locust/locustfile.py
@@ -1,17 +1,28 @@
 from locust import HttpUser, TaskSet, task, between
 import json
 import time
-
+from json import JSONDecodeError
 class UserBehavior(TaskSet):
     @task
     def create_post(self):
-        headers = {'content-type': 'application/json','Accept-Encoding':'gzip'}
-        body = json.dump({"audio": [{"audioUri": "https://storage.googleapis.com/test_public_bucket/download.wav"}]})
-        self.client.post('/en', data = body, headers=headers)
+        headers = {'content-type': 'application/json'}
+        body1 = {"audio": [{"audioUri": "https://www2.engr.arizona.edu/~429rns/audiofiles/cutafew.wav"}]}
+        body = json.dumps({"audio": [{"audioUri": "https://www2.engr.arizona.edu/~429rns/audiofiles/cutafew.wav"}]})
+        with self.client.post('/asr/v1/recognize/en', data = body, headers = headers,catch_response=True) as response:
+            try:
+                if ((response.json()["status"] == "SUCCESS") and (response.status_code == 200)):
+                    response.success()
+                else:
+                    response.failure("Response code:" + str(response.status_code) +" And Response:" +str(response.json()))
+            except JSONDecodeError:
+                response.failure("Response could not be decoded as JSON " + "Response code:" + str(response.status_code))
+            except KeyError:
+                response.failure("Response did not contain expected key 'status' " + "Response code:" + str(response.status_code))
+
         # headers=headers,
         # name = "Create a new post")
 #
 class WebsiteUser(HttpUser):
     tasks = [UserBehavior]
-    wait_time = between(5, 10)
+    # wait_time = between(5, 10)
 #         response = self.client.post("/post/endpoint", data=json.dumps(payload), headers=headers)
